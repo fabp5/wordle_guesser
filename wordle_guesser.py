6a3635b12c1885ab791d5623c188177aa05a164b
Freya Boardman-Pretty <freyabp@gmail.com>
2022-02-02 20:29:30 +0000

    add script

---
+++ wordle_guesser.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Wordle guesser script
+"""
+
+import nltk.corpus
+import re
+
+from nltk.corpus import words
+words.words()
+allwords = words.words().copy()
+
+fivelwords = [word for word in allwords if len(word) == 5]
+
+# get the known letters
+while True:
+    known_letters = input("Enter the word with letters where their position is known, and underscores where the letter is not known (e.g. 'F_B___'): " )
+    if (len(known_letters) != 5) or not known_letters.replace("_", "").isalpha():
+        print("Input must be 5 characters long and consist of only letters and underscores")
+        continue
+    else:
+        break
+
+# get potential letters at each position
+known_letters = list(known_letters)
+for i in range(0,5):
+    if known_letters[i] == "_":
+        known_letters[i] = "abcdefghijklmnopqrstuvwxyz"
+
+# get letters that are present but not in certain positions
+while True:
+    other_letters = input("For each position enter the letters not present here but present elsewhere in the word, using an underscore if none, separated by commas (e.g. _,t,_,_,io) :")
+    if (other_letters.count(",") != 4) or not re.sub("_|,","",other_letters).isalpha():
+        print(other_letters.count(","))
+        print(other_letters.replace("_|,", ""))
+        print("Input must be five strings of letters or underscores, separated by commas")
+        continue
+    else:
+        break
+        
+other_letters = other_letters.lower()
+letters_in = other_letters
+other_letters = other_letters.split(",")
+for i in range(len(other_letters)):
+    other_letters[i] = other_letters[i].replace("_","")
+
+letters_in = re.sub("_|,","",letters_in)
+letters_in = set(letters_in)
+
+# get letters not in the word
+while True:
+    letters_out = input("Enter the letters NOT in the word as one string (e.g. 'ertls'), or leave blank if none:")
+    if not letters_out.isalpha() and letters_out != "":
+        print("Input must be a string of letters or blank")
+        continue
+    else:
+        break
+letters_out = set(letters_out)
+
+# filter for words with the known letters at these positions
+filtered_known = [word for word in fivelwords
+          if (word[0] in known_letters[0] and
+              word[1] in known_letters[1] and
+              word[2] in known_letters[2] and
+              word[3] in known_letters[3] and
+              word[4] in known_letters[4] and
+              all(x in word for x in letters_in) and
+              all(y not in word for y in letters_out))]
+          
+# filter to words without the "other" letters at those positions
+filtered_all = [word for word in filtered_known
+             if(word[0] not in other_letters[0] and
+                word[1] not in other_letters[1] and
+                word[2] not in other_letters[2] and
+                word[3] not in other_letters[3] and
+                word[4] not in other_letters[4])]
+print("Suggested words: ",filtered_all)
+
